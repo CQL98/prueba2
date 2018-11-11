f319c52c8b4eb31961f4485eda9d96a1e19eeef2
CQL98 <quispeLuna@outlook.com>
2018-11-11 00:04:37 -0500

    mod datos
    mod datos

--- Sistema/Sistema/WebClientes.aspx.vb
+++ Sistema/Sistema/WebClientes.aspx.vb
@@ -3,20 +3,31 @@ Imports CapaNegocio
 
 Public Class WbClientes
     Inherits System.Web.UI.Page
-
+    Dim mpContentPlaceHolder As ContentPlaceHolder
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        mpContentPlaceHolder = CType(Master.FindControl("ContentPlaceHolder1"), ContentPlaceHolder)
+        If Not mpContentPlaceHolder Is Nothing Then
+            If Not IsPostBack Then
+                mostrar()
+            End If
 
-        If Not IsPostBack Then
-            mostrar()
         End If
+
+
+
+
     End Sub
 
     Sub mostrar()
         Dim oCliente As New ClienteCN
+        Dim grdCliente As GridView
+        grdCliente = CType(mpContentPlaceHolder.FindControl("grdCliente"), GridView)
+        grdCliente.DataSource = oCliente.ListarTodos
+        grdCliente.DataBind()
 
-        'GrdClientes.DataSource = oCliente.ListarTodos
-        'GrdClientes.DataBind()
+
+
     End Sub
 
 End Class
